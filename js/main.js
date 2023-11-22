8ccb4da144a102cccc4423b2bf783e0be6d668d3
juanMa Gaitan <jmgaitan93@gmail.com>
2023-11-22 01:18:03 -0300

    parte del punto 3

--- js/main.js
+++ js/main.js
@@ -3,3 +3,27 @@
 console.log(document.title)
 
 // Leemos datos definidos en HTML
+
+
+var integrantes = [];
+
+classByIntegrantes = document.getElementsByClassName('integrantes')
+console.log('mostramos objeto en posicion 0')
+for (let index = 0; index < classByIntegrantes.length; index++) {
+    console.log(classByIntegrantes[index])
+
+        var integrante = {};
+        console.log('arranca armado del array')
+        integrante.primerNombre = classByIntegrantes[index].children[1].innerHTML
+   
+        integrante.segundoNombre = classByIntegrantes[index].children[3].innerHTML
+   
+        integrante.primerApellido= classByIntegrantes[index].children[5].innerHTML
+   
+        integrante.segundoApellido = classByIntegrantes[index].children[7].innerHTML
+   
+        integrantes.push(integrante)
+        console.log(integrantes)
+}
+
+
